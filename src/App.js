83c93145f1d1b497ace79a85038c6a12f3e2fa02
Sam Thomson <samt@samt.st>
2018-12-05 11:28:35 +0700

    pull off and display more data

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ import { timingSafeEqual } from 'crypto';
 class App extends Component {
 
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: ''
   }
 
   async componentDidMount () {
     const manager = await lottery.methods.manager().call()
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
     
-    this.setState({ manager })
+    this.setState({
+      manager,
+      players,
+      balance
+    })
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}.</p>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entereed competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether
+        </p>
       </div>
     );
   }
