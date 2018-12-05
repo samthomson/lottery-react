9c3cdaeff22a1182eeec82df26fdbcf27cf408f0
Sam Thomson <samt@samt.st>
2018-12-05 11:20:21 +0700

    pull data off from contract

--- src/lottery.js
+++ src/lottery.js
@@ -1,8 +1,7 @@
 import web3 from './web3'
-const envVars = require('./env')
+import envVars from './env'
 
 const address = envVars.address
-
 const abi = envVars.ABI
 
 export default new web3.eth.Contract(abi, address)
